3bf465dc9486b360bf962d1fdb405609fba5697b
Bi <bzs2001@163.com>
2017-08-13 18:04:32 +0800

    更新curl

--- src/Curl.php
+++ src/Curl.php
@@ -31,7 +31,7 @@ class Curl
 	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);	
 	    curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);	  
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
 		
 	    $contents = curl_exec($ch);
 	    curl_close($ch);
@@ -40,8 +40,8 @@ class Curl
 
 	public static function curl_https($url,$header=[])
 	{
-		$curl = curl_init();
-		$curl = curl_init();
+		$ch = curl_init();
+		
 		curl_setopt($curl, CURLOPT_URL, $url);
 		if($header) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
@@ -50,6 +50,9 @@ class Curl
 		// 当请求https的数据时，会要求证书，这时候，加上下面这两个参数，规避ssl的证书检查
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
 		$contents = curl_exec($ch);
 	    curl_close($ch);
 	    return  $contents;
